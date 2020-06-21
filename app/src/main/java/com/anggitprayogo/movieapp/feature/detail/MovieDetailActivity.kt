38362f25860e877cff44b8ce904eab6ec9c55941
anggitprayogo <anggitprayogo@gmail.com>
2020-06-21 16:50:39 +0700

    anchor fab to appbar

--- app/src/main/java/com/anggitprayogo/movieapp/feature/detail/MovieDetailActivity.kt
+++ app/src/main/java/com/anggitprayogo/movieapp/feature/detail/MovieDetailActivity.kt
@@ -1,17 +1,20 @@
 package com.anggitprayogo.movieapp.feature.detail
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.anggitprayogo.core.base.BaseActivity
 import com.anggitprayogo.core.util.ext.load
-import com.anggitprayogo.core.util.viewmodel.ViewModelFactory
 import com.anggitprayogo.movieapp.BaseApplication
 import com.anggitprayogo.movieapp.R
 import com.anggitprayogo.movieapp.data.entity.MovieDetail
+import com.google.android.material.appbar.AppBarLayout
 import kotlinx.android.synthetic.main.activity_movie_detail.*
 import javax.inject.Inject
+
 
 class MovieDetailActivity : BaseActivity() {
 
@@ -19,7 +22,11 @@ class MovieDetailActivity : BaseActivity() {
     lateinit var viewModelFactory: ViewModelProvider.Factory
     private lateinit var viewModel: MovieDetailViewModel
 
+    private var movieDetail: MovieDetail? = null
+
     private var movieId: String? = null
+
+    private var menu: Menu? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         initInjector()
@@ -30,6 +37,65 @@ class MovieDetailActivity : BaseActivity() {
         initObserver()
         initListener()
         fetchData()
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        appBarLayout.addOnOffsetChangedListener(object : AppBarLayout.OnOffsetChangedListener {
+            var isShow = false
+            var scrollRange = -1
+            override fun onOffsetChanged(appBar: AppBarLayout?, verticalOffset: Int) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.totalScrollRange
+                }
+                if ((scrollRange + verticalOffset) == 0) {
+                    isShow = true
+                    showMenuFavourite(R.id.action_favourite);
+                } else if (isShow) {
+                    isShow = false
+                    hideMenuFavourite(R.id.action_favourite);
+                }
+            }
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        this.menu = menu
+        menuInflater.inflate(R.menu.menu_detail, menu)
+        hideMenuFavourite(R.id.action_favourite)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (id == R.id.action_favourite) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    @Suppress("SameParameterValue")
+    private fun hideMenuFavourite(id: Int) {
+        val item = menu?.findItem(id)
+        item?.isVisible = false
+        setActionBarTitle(null)
+    }
+
+    @Suppress("SameParameterValue")
+    private fun showMenuFavourite(id: Int) {
+        val item = menu?.findItem(id)
+        item?.isVisible = true
+        setActionBarTitle(movieDetail?.title)
+    }
+
+    private fun setActionBarTitle(title: String?) {
+        supportActionBar?.title = title
     }
 
     private fun handleIntent() {
@@ -57,12 +123,21 @@ class MovieDetailActivity : BaseActivity() {
     }
 
     private fun handleStateMovieDetail(movie: MovieDetail) {
+        movieDetail = movie
         ivBannerMovie.load(movie.getBannerMovie())
         tvMovieMetaData.text = movie.getMetaData()
         tvMovieTitle.text = movie.originalTitle
-        tvImdbRating.text = getString(R.string.imdb_rating_template, (movie.getImdbRating()?.toFloat() ?: 0.0).toString())
+        tvImdbRating.text = getString(
+            R.string.imdb_rating_template,
+            (movie.getImdbRating()?.toFloat() ?: 0.0).toString()
+        )
         ratingMovie.rating = movie.getImdbRating()?.toFloat() ?: 0f
         tvOverview.text = movie.overview
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 
     override fun initInjector() {
